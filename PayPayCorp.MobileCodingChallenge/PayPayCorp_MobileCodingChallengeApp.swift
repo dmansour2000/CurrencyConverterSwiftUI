8012b67eae625ea2819d5447966343f23f330ea0
dmansour83 <abdelkareem.dina@gmail.com>
2023-11-18 09:26:34 +0200

    Initial Commit

---
+++ PayPayCorp.MobileCodingChallenge/PayPayCorp_MobileCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPayCorp_MobileCodingChallengeApp.swift
+//  PayPayCorp.MobileCodingChallenge
+//
+//  Created by Dina  on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PayPayCorp_MobileCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
